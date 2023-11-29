ef5a601d324a6b2e2709c4318a31b554e03cff54
fenrirsoftware <fenrirsoftware@protonmail.com>
2023-11-29 15:33:37 +0300

    Proje dosyası ekle.

---
+++ PasswordFinder/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace PasswordFinder
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(42, 38);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(285, 33);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(85, 179);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(196, 64);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Password";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBox1.Location = new System.Drawing.Point(42, 125);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(285, 44);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(150, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Wİ-Fİ Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(155, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(372, 255);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Wifi Password Finder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ PasswordFinder/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PasswordFinder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UpdateProfileList();
+        }
+
+        private void UpdateProfileList()
+        {
+            // netsh komutunu çalıştır ve çıktısını oku
+            string output = RunNetshCommand("wlan show profiles");
+
+            // Profil isimlerini çıktıdan al ve combobox'a ekle
+            string[] profiles = GetProfileNames(output);
+            comboBox1.Items.Clear();
+            comboBox1.Items.AddRange(profiles);
+        }
+
+        private string RunNetshCommand(string arguments)
+        {
+            try
+            {
+                ProcessStartInfo processStartInfo = new ProcessStartInfo
+                {
+                    FileName = "netsh",
+                    Arguments = arguments,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                using (Process process = new Process { StartInfo = processStartInfo })
+                {
+                    process.Start();
+                    string output = process.StandardOutput.ReadToEnd();
+                    process.WaitForExit();
+                    return output;
+                }
+            }
+            catch (Exception ex)
+            {
+                return $"Error: {ex.Message}";
+            }
+        }
+
+        private string[] GetProfileNames(string netshOutput)
+        {
+            // netsh çıktısından profil isimlerini ayıkla
+            string[] lines = netshOutput.Split('\n');
+            System.Collections.Generic.List<string> profiles = new System.Collections.Generic.List<string>();
+
+            foreach (string line in lines)
+            {
+                if (line.Contains("All User Profile"))
+                {
+                    // "All User Profile" içeren satırdan profil adını çıkar
+                    int startIndex = line.IndexOf(":") + 1;
+                    string profileName = line.Substring(startIndex).Trim();
+                    profiles.Add(profileName);
+                }
+            }
+
+            return profiles.ToArray();
+        }
+
+    
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // "Key'i Göster" butonuna tıklandığında seçilen profille ilgili key'i al ve TextBox'a yazdır
+            string selectedProfile = comboBox1.SelectedItem as string;
+            if (!string.IsNullOrEmpty(selectedProfile))
+            {
+                // netsh komutunu çalıştır ve çıktısını oku
+                string commandOutput = RunNetshCommand($"wlan show profiles name=\"{selectedProfile}\" key=clear");
+
+                // "Key Content" değerini al
+                string keyContent = GetKeyContent(commandOutput);
+
+                // TextBox'a yazdır
+                textBox1.Text = keyContent;
+            }
+        }
+
+        private string GetKeyContent(string netshOutput)
+        {
+            // netsh çıktısından "Key Content" değerini ayıkla
+            string[] lines = netshOutput.Split('\n');
+
+            foreach (string line in lines)
+            {
+                if (line.Contains("Key Content"))
+                {
+                    // "Key Content" içeren satırdan değeri çıkar
+                    int startIndex = line.IndexOf(":") + 1;
+                    string keyContent = line.Substring(startIndex).Trim();
+                    return keyContent;
+                }
+            }
+
+            return "Key Content not found.";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
